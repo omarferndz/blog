d0060d92771a8dda54cb6129245c1bc3bd326f9a
Omar Fernandez <omarferndz@gmail.com>
2019-02-28 13:02:55 -0400

    13incorporacion de laravel collective para crear formularios, terminacion de index, funcionalidad al boton eliminar

--- resources/views/admin/tags/index.blade.php
+++ resources/views/admin/tags/index.blade.php
@@ -41,7 +41,12 @@
                                             </a>
                                         </td>
                                         <td width="10px">
-                                            Eliminar
+                                            {!! Form::open(['route' => ['tags.destroy', $tag->id], 
+                                            'method' => 'DELETE']) !!}
+                                                <button class="btn btn-sm btn-danger">
+                                                    Eliminar
+                                                </button>        
+                                            {!! Form::close() !!}
                                         </td>
                                     </tr>
                                 @endforeach
