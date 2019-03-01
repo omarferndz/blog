a452460998a77db84e5df5fb5e6cd8f38bf6e718
omarferndz <omarferndz@gmail.com>
2019-03-01 17:10:34 -0400

    19completar formulario de los posts

--- resources/views/admin/posts/partials/form.blade.php
+++ resources/views/admin/posts/partials/form.blade.php
@@ -14,6 +14,33 @@
     {{ Form::text('slug', null, ['class' => 'form-control', 'id' => 'slug']) }}
 </div>
 <div class="form-group">
+    {{ Form::label('file', 'Imagen') }}
+    {{ Form::file('file') }}
+</div>
+<div class="form-group">
+    {{ Form::label('status', 'Estado') }}
+    <label>
+        {{ Form::radio('status', 'PUBLISHED' ) }} Publicado
+    </label>
+    <label>
+        {{ Form::radio('status', 'DRAFT' ) }} Borrador
+    </label>
+</div>
+<div class="form-group">
+    {{ Form::label('tags', 'Etiquetas') }}
+    <div>
+        @foreach($tags as $tag)
+            <label>
+                {{ Form::checkbox('tags[]', $tag->id) }} {{ $tag->name }}
+            </label>
+        @endforeach
+    </div>
+</div>
+<div class="form-group">
+    {{ Form::label('excerpt', 'Extracto') }}
+    {{ Form::textarea('excerpt', null, ['class' => 'form-control', 'rows' => '2']) }}
+</div>
+<div class="form-group">
     {{ Form::label('body', 'Descripcion' )}}
     {{ Form::textarea('body', null, ['class' => 'form-control']) }}
 </div>
