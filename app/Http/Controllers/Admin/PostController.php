0f2c17817e88e482ce10af2b25148d3f4c146540
omarferndz <omarferndz@gmail.com>
2019-03-01 15:43:18 -0400

    17creacion de modulo de entradas CRUD

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\PostStoreRequest;
+use App\Http\Requests\PostUpdateRequest;
+
 use App\Http\Controllers\Controller;
+
+use App\Post;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +23,9 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::orderBy('id', 'DESC')->paginate();
+        
+        return view('admin.posts.index', compact('posts'));
     }
 
     /**
@@ -24,7 +35,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view ('admin.posts.create'); 
     }
 
     /**
@@ -33,9 +44,12 @@ class PostController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PostStoreRequest $request)
     {
-        //
+        $post = Post::create($request->all());
+
+        return redirect()->route('posts.edit', $post->id)
+            ->with('info', 'Entrada creada con exito');
     }
 
     /**
@@ -46,7 +60,9 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        //
+        $post = Post::find($id);
+
+        return view('admin.posts.show', compact('post'));        
     }
 
     /**
@@ -57,7 +73,9 @@ class PostController extends Controller
      */
     public function edit($id)
     {
-        //
+        $post = Post::find($id);
+
+        return view('admin.posts.edit', compact('post')); 
     }
 
     /**
@@ -67,9 +85,14 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(PostUpdateRequest $request, $id)
     {
-        //
+        $post = Post::find($id);
+
+        $post->fill($request->all())->save();
+
+        return redirect()->route('posts.edit', $post->id)
+        ->with('info', 'Entrada actualizada con exito');
     }
 
     /**
@@ -80,6 +103,8 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id)->delete();
+
+        return back()->with('info', 'Eliminado correctamente');
     }
 }
